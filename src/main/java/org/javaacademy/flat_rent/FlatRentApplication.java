bc644e25340a5a069f50a624c99d573b93da3028
firebolt182 <firebolt182@mail.ru>
2024-06-27 22:02:41 +0300

    init commit

---
+++ src/main/java/org/javaacademy/flat_rent/FlatRentApplication.java
@@ -0,0 +1,13 @@
+package org.javaacademy.flat_rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/javaacademy/flat_rent/FlatRentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaacademy.flat_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
